dac0093a10e7e33f6f6b74ea2bb3b9d236b2153c
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-01-26 01:13:46 +0900

    updated

--- scripts/lib/observer.js
+++ scripts/lib/observer.js
@@ -1,10 +1,20 @@
+const  titles = document.querySelectorAll('.section-title');
+
 const cb_sv = function(entries, observer) {
   entries.forEach(entry => {
     if(entry.isIntersecting) {
+      // console.log('slidebox and voice inview');
       entry.target.classList.add('inview');
       observer.unobserve(entry.target);
     } else {
+      // console.log('slidebox and voice out view');
       entry.target.classList.remove('inview');
     }
   });
 }
+
+const options = {
+  rootMargin: "-200px 0px",
+}
+const io_sv = new IntersectionObserver(cb_sv, options);
+titles.forEach(title =>io_sv.observe(title));
