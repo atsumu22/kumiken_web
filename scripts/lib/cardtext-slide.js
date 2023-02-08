ab0da4fac339e65af99972a75e2f536f911e7bff
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-02-08 13:34:35 +0900

    touchstart

--- scripts/lib/cardtext-slide.js
+++ scripts/lib/cardtext-slide.js
@@ -1,7 +1,16 @@
+const isTouchable = "ontouchstart" in window || (window.DocumentTouch && document instanceof DocumentTouch);
 const textboxes = document.querySelectorAll(".card-works__text");
 
-textboxes.forEach((textbox) => {
-  textbox.addEventListener("click", (event) => {
-    event.currentTarget.classList.toggle('clicked');
+if (isTouchable) {
+  textboxes.forEach((textbox) => {
+    textbox.addEventListener("touchstart", (event) => {
+      event.currentTarget.classList.toggle('clicked');
+    });
   });
-});
+} else {
+  textboxes.forEach((textbox) => {
+    textbox.addEventListener("click", (event) => {
+      event.currentTarget.classList.toggle('clicked');
+    });
+  });
+}
