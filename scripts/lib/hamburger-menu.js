b04b430748328f1d407a5145117d42ba88d491da
Junsuke Watanabe <w.atsumu27@gmail.com>
2023-02-08 13:31:50 +0900

    updated

--- scripts/lib/hamburger-menu.js
+++ scripts/lib/hamburger-menu.js
@@ -1,13 +1,22 @@
+const isTouchable = "ontouchstart" in window || (window.DocumentTouch && document instanceof DocumentTouch);
 const hamburger = document.querySelector(".hamburger")
 const hidden_menu = document.querySelector(".hamburger-hidden")
 const navbar = document.querySelector(".navbar")
 const link_tags = document.querySelectorAll(".link-tag")
 
-hamburger.addEventListener("click", (event) => {
-  event.currentTarget.classList.toggle('menu-open');
-  hidden_menu.classList.toggle('menu-open');
-  navbar.classList.toggle('menu-open');
-});
+if (isTouchable) {
+  hamburger.addEventListener("touchstart", (event) => {
+    event.currentTarget.classList.toggle('menu-open');
+    hidden_menu.classList.toggle('menu-open');
+    navbar.classList.toggle('menu-open');
+  });
+} else {
+  hamburger.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle('menu-open');
+    hidden_menu.classList.toggle('menu-open');
+    navbar.classList.toggle('menu-open');
+  });
+}
 
 link_tags.forEach((tag) => {
   tag.addEventListener("click", (event) => {
